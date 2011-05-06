f60584e96cf238b85b7b039961aec9c3eb14baa3
JaviSanromán <sanroman.javier@gmail.com>
2011-05-07 00:43:59 +0200

    Nombre clases modelo

--- application/models/dato.php
+++ application/models/dato.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-class ccaa extends CI_Model {
+class dato extends CI_Model {
 	
 	var $id = 0;
 	var $localidad_id = '';
--- application/models/provincia.php
+++ application/models/provincia.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-class ccaa extends CI_Model {
+class provincia extends CI_Model {
 	
 	var $id = 0;
 	var $nombre = '';

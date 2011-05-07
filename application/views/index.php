b3d783872691712b33f5f9c2e4b6301b1346a714
JaviSanromán <sanroman.javier@gmail.com>
2011-05-07 13:40:28 +0200

    Parte maqueta home

--- application/views/index.php
+++ application/views/index.php
@@ -1,10 +1,28 @@
-<div style="background:#cccccc;">
+<div id="index">
 
 
 <div id="map_canvas"></div>
 
 
-LA HOME
+<div class="block red">
+
+	<div class="title">... en España</div>
+
+	<div style="text-align:center;">
+		<div class="text_right inlineblock">
+			<div class="cifra"><spam class="red">4.000.000</spam></div>
+			parados
+		</div> 
+		<div class="text_right inlineblock">
+			<div class="cifra"><spam class="green">4.000.000</spam></div>
+			nuevos contratos
+		</div> 
+		<div class="text_right inlineblock">
+			<div class="cifra"><spam class="green">4.000.000</spam></div>
+			ofertas
+		</div>
+	</div> 
+</div>
 
 
 </div>

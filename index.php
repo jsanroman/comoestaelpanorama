aba775bf9771cafd2367f5cd3732d82e0c64d37f
JaviSanromán <sanroman.javier@gmail.com>
2011-05-07 10:50:08 +0200

    config log, timezone

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set('Europe/Madrid');
+
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
@@ -18,7 +20,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-	define('ENVIRONMENT', 'development');
+	define('ENVIRONMENT', 'testing');
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING

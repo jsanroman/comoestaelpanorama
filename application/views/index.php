9ab0df871a385ba75b07339def05135497ca0a27
JaviSanromán <sanroman.javier@gmail.com>
2011-05-07 15:12:36 +0200

    Jugando con css

--- application/views/index.php
+++ application/views/index.php
@@ -4,7 +4,7 @@
 <div id="map_canvas"></div>
 
 
-<div class="block red">
+<div class="block red_background">
 
 	<div class="title">... en España</div>
 
@@ -14,15 +14,29 @@
 			parados
 		</div> 
 		<div class="text_right inlineblock">
-			<div class="cifra"><spam class="green">4.000.000</spam></div>
+			<div class="cifra"><spam class="green">4.000</spam></div>
 			nuevos contratos
 		</div> 
 		<div class="text_right inlineblock">
-			<div class="cifra"><spam class="green">4.000.000</spam></div>
+			<div class="cifra"><spam class="green">10.000</spam></div>
 			ofertas
 		</div>
 	</div> 
 </div>
 
 
+<div class="block blue_background" style="margin-top:20px;">
+
+	<div class="title">... y en tu zona?</div>
+
+	<div">
+
+		<input type="text" />
+		<input type="submit" value="Buscar"/> 
+
+	</div> 
+</div>
+
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 </div>
